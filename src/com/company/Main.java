9437977c27e3902ec7c5052c17883ed3eb7b5d65
Gerjak Balazs <bmailggreen@gmail.com>
2021-07-12 19:17:58 +0200

    Final version.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("3 + 2 = " + calc.add(3,2));
+        System.out.println("3 - 2 = " + calc.sub(3,2));
+        System.out.println("3 * 2 = " + calc.multiple(3,2));
+        System.out.println("3 / 2 = " + calc.osztas(3,2));
 
     }
 }
